7fe7fc0ddf8e02f3e0c685d16c840a8ea1913c10
bizzyd123 <wendy_cone_@hotmail.com>
2024-11-19 02:54:05 +0000

    examen terminado

---
+++ RTA_Examen_20241115/Punto_B.sh
@@ -0,0 +1,6 @@
+cd /usr/local/bin/
+sudo nano ChiquipaLeonAltaUser-Groups.sh
+
+ sudo /usr/local/bin/ChiquipaLeonAltaUser-Groups.sh 2P_202406_Prog1 /home/vagrant/UTN-FRA_SO_Examenes/202406/bash_script/Lista_Usuarios.txt
+
+ cat /etc/group | grep 2P_
